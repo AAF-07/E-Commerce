73924c7ab84b76247b51a959af725de7bfe3c8c7
Aziz <firmansyahaziz38@gmail.com>
2026-02-09 07:29:16 +0700

    login form

---
+++ resources/views/Staff/login.blade.php
@@ -0,0 +1,14 @@
+<h1>BookShelf</h1>
+
+<div class="">
+    <form action="login">
+        @csrf
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required>
+        <br>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required>
+        <br>
+        <button type="submit">Login</button>
+    </form>
+</div>
